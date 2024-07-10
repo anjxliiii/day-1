7b092f0043c7e66e7e62d44bc495faee3fa98d58
anjxliiii <anjalinowshad@gmail.com>
2024-07-10 16:23:44 +0530

    welcome

---
+++ script.js
@@ -0,0 +1,34 @@
+a=5;
+var a=9;
+console.log(a);
+{
+let b=20;
+console.log(b)
+const t=10
+}
+console.log(typeof(b));
+var st='I am a string';
+var d=true;
+var m;
+var arr1=['orange','Apple',10]
+function add(x,y){
+    var sum=x+y;
+    console.log(sum)
+}
+var p=add(11,30);
+console.log(p);
+var e=10;
+var f='10';
+if (e>f) {
+    console.log('e is greater')
+}
+else if(e==f){
+    console.log('e is equal to f')
+}
+else {
+    console.log('f is greater')
+}
+
+
+ 
+
